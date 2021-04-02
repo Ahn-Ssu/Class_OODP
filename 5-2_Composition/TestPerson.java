b794175088fb1bed764e62383c20980418321cde
Ahn-Ssu <64300241+Ahn-Ssu@users.noreply.github.com>
2021-04-02 13:32:39 +0900

    Add files via upload

---
+++ 5-2_Composition/TestPerson.java
@@ -0,0 +1,8 @@
+//package composition;
+public class TestPerson {
+    public static void main(String[] args) {
+        Person person = new Person();
+        long salary = person.getSalary();
+        System.out.println("Salary"+salary);
+    }
+}
