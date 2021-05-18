2d141ce37c262b66cf9f75cb02d2d9b40d0584cb
Ahn-Ssu <64300241+Ahn-Ssu@users.noreply.github.com>
2021-05-18 13:26:55 +0900

    Add files via upload

---
+++ 1_LectureCode/12-1_ShallowCopy/Ex.java
@@ -0,0 +1,11 @@
+	import java.util.*;
+	public class Ex {
+	    private int[] data;
+	    // makes a shallow copy of values
+	    public Ex(int[] values) {
+	        data = values;
+	    }
+	    public void showData() {
+			System.out.println( Arrays.toString(data) );
+	    }
+	}
